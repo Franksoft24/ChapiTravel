2ed264fa421d9723845a3ba86bda708bf0173fc7
Jalissa <jalissa2403@gmail.com>
2014-12-03 01:39:17 +0000

    Recuperando cambios de ServicioDAO.php

--- daos/ServicioDAO.php
+++ daos/ServicioDAO.php
@@ -8,20 +8,20 @@ class ServicioDAO extends DataAccessObject{
 
 	public function delete(Servicio $entidad){
 		$this->data->delete(Servicio::TABLE_NAME, 
-			array('idservicio' => $entidad->getIdservicio()));
+			array('ServicioID' => $entidad->getServicioID()));
 		return $this->data->result();
 	}
 
 	public function update(Servicio $entidad){
 		$this->data->update(Servicio::TABLE_NAME, $entidad->toArray(), 
-			array('idservicio' => $entidad->getIdservicio()));
+			array('ServicioID' => $entidad->getServicioID()));
 		return $this->data->result();
 	}
 
 	public function getByID(Servicio $entidad){
 		$this->data->get($entidad::TABLE_NAME);
 
-		$whereArray = array('idservicio = ' => $entidad->getIdservicio());
+		$whereArray = array('ServicioID = ' => $entidad->getServicioID());
 		$this->data->where($whereArray);
 
 		return $this->data->result();
